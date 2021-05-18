1d7e946a3b1439d68699614f6a09887f231a6ccf
윤이진 <483759@naver.com>
2021-05-19 06:08:55 +0900

    글 목록 조회 구현중

--- src/store/index.js
+++ src/store/index.js
@@ -8,21 +8,45 @@ export default new Vuex.Store({
   state: {
     login: "1",
     member: Object,
+    boards: [],
+    boardPageNav: {},
+    word: "",
+    key: "",
   },
-  getters: {},
+  getters: {
+    requestParams(state) {
+      return {
+        key: state.key,
+        word: state.word,
+        pg: state.boardPageNav.currentPage ?? 1,
+      };
+    },
+  },
   mutations: {
     JOIN_MEMBER(state, member) {
       state.member = member;
     },
     LOGIN_MEMBER(state, member) {
       state.member = member;
-      state.login= "2";
+      state.login = "2";
       console.log(state.member);
     },
     LOGOUT_MEMBER(state) {
-      state.login= "1";
+      state.login = "1";
       console.log(state.login);
-    }
+    },
+    SET_BOARDS(state, payload) {
+      state.boards = payload;
+    },
+    SET_KEY(state, payload) {
+      state.key = payload;
+    },
+    SET_WORD(state, payload) {
+      state.word = payload;
+    },
+    SET_BOARDPAGENAV(state, payload) {
+      state.boardPageNav = payload;
+    },
   },
   actions: {
     joinMember({ commit }, member) {
@@ -48,8 +72,23 @@ export default new Vuex.Store({
           console.dir(error);
         });
     },
+    getArticleList({ commit }, params) {
+      console.log("call getBoards");
+      commit("SET_KEY", params.key);
+      commit("SET_WORD", params.word);
+      axios
+        .get("/board", { params })
+        .then((response) => {
+          console.log(response);
+          commit("SET_BOARDS", response.data.list);
+          commit("SET_BOARDPAGENAV", response.data.navigation);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     logoutMember({ commit }) {
       commit("LOGOUT_MEMBER");
-    }
+    },
   },
 });

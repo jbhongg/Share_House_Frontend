13a44ada91a4f3a1ecff8d3bc64ba3a43c872963
홍진빈 <jbhongg@naver.com>
2021-05-23 00:29:41 +0900

    5

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:80',
+                target: 'http://localhost:8092',
                 changeOrigin: true,
                 logLevel: 'debug',
             },

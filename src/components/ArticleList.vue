8681e7720652e26ec0b115ab5237ea4c10e9ada7
윤이진 <483759@naver.com>
2021-05-25 19:29:10 +0900

    게시판 페이징처리

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,37 +1,70 @@
 <template>
   <div>
-    <b-container v-if="typeof(boards) !='undefined'" class="container">
-	<table class="table table-borderless">
-	  	<tr>
-	  		<td align="right"><button type="button" id="mvWriteBtn" class="btn btn-link" v-if="login !== '1'" @click="$router.push({name: 'ArticleWrite'})">글쓰기</button></td>
-	  	</tr>
-	  </table>
-      <b-table id="board_table" :items="boards" :fields="fields" >
-	   <template #cell(update)="articleno">
-        <b-button v-if="articleno.item.userid === member.data.id" size="sm" @click="$router.push({name: 'ArticleUpdate', params: {no: articleno.item.articleno}})" class="mr-2">수정
-        </b-button>
-      </template>
-	  	<template #cell(delete)="articleno">
-        <b-button v-if="articleno.item.userid === member.data.id" size="sm" @click="deleteBoard(articleno)" class="mr-2">삭제
-        </b-button>
-      </template>
-	  </b-table>
-	  
-      <b-pagination v-if="typeof(boardPageNav) != 'undefined'" 
+    <b-container v-if="typeof boards != 'undefined'" class="container">
+      <table class="table table-borderless">
+        <tr>
+          <td align="right">
+            <button
+              type="button"
+              id="mvWriteBtn"
+              class="btn btn-link"
+              v-if="login !== '1'"
+              @click="$router.push({ name: 'ArticleWrite' })"
+            >
+              글쓰기
+            </button>
+          </td>
+        </tr>
+      </table>
+      <b-table id="board_table" :items="boards" :fields="fields">
+        <template #cell(update)="articleno">
+          <b-button
+            v-if="articleno.item.userid === member.data.id"
+            size="sm"
+            @click="
+              $router.push({ name: 'ArticleUpdate', params: { no: articleno.item.articleno } })
+            "
+            class="mr-2"
+            >수정
+          </b-button>
+        </template>
+        <template #cell(delete)="articleno">
+          <b-button
+            v-if="articleno.item.userid === member.data.id"
+            size="sm"
+            @click="deleteBoard(articleno)"
+            class="mr-2"
+            >삭제
+          </b-button>
+        </template>
+      </b-table>
+
+      <b-pagination
+        v-if="typeof boardPageNav != 'undefined'"
         v-model="boardPageNav.currentPage"
-        :total-rows="boardPageNav.totalPageCount"
+        :total-rows="boardPageNav.totalCount"
         :per-page="boardPageNav.naviSize"
         aria-controls="board_table"
         align="center"
-        @page-move="pageMove"
+        @page-click="pageMove"
       />
     </b-container>
     <b-container v-else class="container">
       <table class="table table-borderless">
-	  	  <tr>
-	  		  <td align="right"><button type="button" id="mvWriteBtn" class="btn btn-link" v-if="login !== '1'" @click="$router.push({name: 'ArticleWrite'})">글쓰기</button></td>
-	  	  </tr>
-	    </table>
+        <tr>
+          <td align="right">
+            <button
+              type="button"
+              id="mvWriteBtn"
+              class="btn btn-link"
+              v-if="login !== '1'"
+              @click="$router.push({ name: 'ArticleWrite' })"
+            >
+              글쓰기
+            </button>
+          </td>
+        </tr>
+      </table>
       <h3>글 많이 써주세요</h3>
     </b-container>
   </div>
@@ -49,8 +82,8 @@ export default {
         { label: "제목", key: "subject" },
         { label: "내용", key: "content" },
         { label: "작성 일자", key: "regtime" },
-		{ label: "수정", key: "update" },
-		{ label: "삭제", key: "delete" },
+        { label: "수정", key: "update" },
+        { label: "삭제", key: "delete" },
       ],
     };
   },
@@ -61,23 +94,23 @@ export default {
     console.log(this.boards);
     this.getdata();
   },
-    watch: {
-    '$route': 'getdata'
+  watch: {
+    $route: "getdata",
   },
   methods: {
     ...mapActions(["getArticleList"]),
-	...mapActions(["deleteArticle"]),
-	getdata(){
-		this.getArticleList(this.$store.getters.requestParams);
-	},
+    ...mapActions(["deleteArticle"]),
+    getdata() {
+      this.getArticleList(this.$store.getters.requestParams);
+    },
     pageMove: function (button, page) {
       const params = { key: this.key, word: this.word, pg: page };
       this.getArticleList(params);
     },
-	deleteBoard(articleno){
-		this.deleteArticle(articleno);
-    this.$router.go();
-	}
+    deleteBoard(articleno) {
+      this.deleteArticle(articleno);
+      this.$router.go();
+    },
   },
 };
 </script>

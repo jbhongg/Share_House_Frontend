509183abaef82801baada4ed86e5faefcc9238de
홍진빈 <jbhongg@naver.com>
2021-05-26 15:34:47 +0900

    헤더사진

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -7,7 +7,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="site-heading">
+          <div class="site-heading" style="padding: 100px 0">
             <h1>SHARE HOUSE</h1>
             <span class="subheading">함께 살아요</span>
           </div>
